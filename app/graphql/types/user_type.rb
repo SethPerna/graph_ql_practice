41fa601ea2683a7c289598d3a94647c4e31caada
Seth Perna <gseth26@gmail.com>
2022-03-21 10:11:10 -0400

    add graphql queries and set up types for user and post

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -7,5 +7,11 @@ module Types
     field :email, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :posts, [Types::PostType], null: true
+    field :posts_count, Integer, null: true
+
+    def posts_count
+      object.posts.count
+    end
   end
 end
